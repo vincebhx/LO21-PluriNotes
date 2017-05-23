19abdedc762594a31b6c708d96fad76082ac4dd4
vincebhx <vince.baheux@gmail.com>
2017-05-23 16:39:35 +0200

    Modif de la structure de Relation

--- src/Relation.h
+++ src/Relation.h
@@ -2,13 +2,13 @@
 #define RELATION_H
 
 #include "Note.h"
-#include "Relation.h"
 #include "Couple.h"
 #include <QString>
 
 // Notes : On interdit la recopie.
 
 class Relation {
+    friend class RelationIterator;
 private:
     QString titre;
     QString description;
@@ -16,11 +16,10 @@ private:
     unsigned int nbCouples; // nombre de couples de type x, y (x, y et y, x) comptent pour deux
     unsigned int nbMaxCouples;
     Couple** couples;
-    void operator=(const Relation& r){}
-    Relation(const Relation& r){}
+    void operator=(const Relation& r);
+    Relation(const Relation& r);
 
 public:
-    friend class RelationIterator;
     static Relation reference;
     Relation(QString t, QString d = NULL, bool o = true): titre(t), description(d), oriente(o) {}
     void setDescription(QString d) { description = d; }
@@ -29,20 +28,21 @@ public:
     QString getDescription() const { return description; }
     bool estOriente() const { return oriente; }
     void addCouple(Couple &c);
-};
 
-class RelationIterator{
-private:
-    const Relation* relation;
-    unsigned int index;
-public:
+    class RelationIterator{
+        friend class Relation;
+    private:
+        const Relation* relation;
+        unsigned int index;
+        RelationIterator(const Relation* rel): relation(rel), index(0) {}
+    public:
+        void first() { index = 0; }
+        void next() { index++; }
+        bool isDone() { if (relation->nbCouples == 0) return true; else return (index + 1 == relation->nbCouples); }
+        Couple* currentItem(){ return relation->couples[index]; }
+    };
 
-    RelationIterator* createIterator(const Relation* rel) { relation = rel; }
-    void first() { index = 0; }
-    void next() { index++; }
-    bool isDone() { if (relation->nbCouples == 0) return 'TRUE'; else return (index + 1 == relation->nbCouples); }
-    Couple* currentItem(){ return relation->couples[index]; }
-
+    RelationIterator* iterator() { return new RelationIterator(this); }
 };
 
 #endif // RELATION_H

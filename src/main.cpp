3802094aede9b550af4d174641c94d4ffc4e17ef
TheLenk <lenaschofield1@gmail.com>
2017-06-06 07:47:04 +0200

    3 tableaux

--- src/main.cpp
+++ src/main.cpp
@@ -17,17 +17,41 @@ int main(int argc, char *argv[])
 
         DbManager& dbM = DbManager::instance();
 
-            QSqlTableModel *model = new QSqlTableModel(0, dbM.db);
-                model->setTable("Article");
-                model->setEditStrategy(QSqlTableModel::OnManualSubmit);
-                model->select();
-                model->setHeaderData(0, Qt::Horizontal, QObject::tr("ID"));
-                model->setHeaderData(1, Qt::Horizontal, QObject::tr("Titre"));
+        QSqlTableModel *modelArticle = new QSqlTableModel(0, dbM.db);
+            modelArticle->setTable("Article");
+            modelArticle->setEditStrategy(QSqlTableModel::OnManualSubmit);
+            modelArticle->select();
+            modelArticle->setHeaderData(0, Qt::Horizontal, QObject::tr("ID"));
+            modelArticle->setHeaderData(1, Qt::Horizontal, QObject::tr("Titre"));
 
-                QTableView *view = new QTableView;
-                view->setModel(model);
-                view->hideColumn(0); // don't show the ID
-                view->show();
+            QTableView *viewArticle = new QTableView;
+            viewArticle->setModel(modelArticle);
+            viewArticle->hideColumn(0); // don't show the ID
+            viewArticle->show();
+
+        QSqlTableModel *modelTache = new QSqlTableModel(0, dbM.db);
+            modelTache->setTable("Tache");
+            modelTache->setEditStrategy(QSqlTableModel::OnManualSubmit);
+            modelTache->select();
+            modelTache->setHeaderData(0, Qt::Horizontal, QObject::tr("ID"));
+            modelTache->setHeaderData(1, Qt::Horizontal, QObject::tr("Titre"));
+
+            QTableView *viewTache = new QTableView;
+            viewTache->setModel(modelTache);
+            viewTache->hideColumn(0); // don't show the ID
+            viewTache->show();
+
+        QSqlTableModel *modelMedia = new QSqlTableModel(0, dbM.db);
+                modelMedia->setTable("Media");
+                modelMedia->setEditStrategy(QSqlTableModel::OnManualSubmit);
+                modelMedia->select();
+                modelMedia->setHeaderData(0, Qt::Horizontal, QObject::tr("ID"));
+                modelMedia->setHeaderData(1, Qt::Horizontal, QObject::tr("Titre"));
+
+                QTableView *viewMedia = new QTableView;
+                viewMedia->setModel(modelMedia);
+                viewMedia->hideColumn(0); // don't show the ID
+                viewMedia->show();
 
         /*DbManager& db = DbManager::instance();
         NotesManager& nm = NotesManager::instance();
